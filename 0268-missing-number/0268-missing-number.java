0902ef204dfdcbcae91edd56ff4552eb88c879f9
Ketan129s <sainiketan129@gmail.com>
2026-07-27 08:15:58 +0530

    Time: 0 ms (100%), Space: 47.2 MB (66.37%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+       int n=nums.length;
+       int x=0;
+       for(int i=0;i<=n;i++)
+       {
+        x^=i;
+       } 
+       for(int num:nums)
+       {
+        x^=num;
+       }
+       return x;
+    }
+}
